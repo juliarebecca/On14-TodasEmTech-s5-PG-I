7d3e0d16ff2c64ede3231e2e09ca532f5bf57d4e
julia <juliaayalarebecca07@gmail.com>
2021-09-24 14:49:23 -0300

    projeto guiado 1

--- projeto/database.js
+++ projeto/database.js
@@ -1,29 +1,83 @@
 //cria o array de objetos estudantes 
 const livros = [
     {
-      nome: 'incluir'
+      nome: 'O menino do pijama listrado - John Boyne',
+      paginas: 216 ,
+      genero: 'romance' , 
+      lido: true
     },
     {
-      nome: 'incluir'
+      nome: 'Olga - Fernando Moraes' ,
+      paginas: 264 ,
+      genero: 'drama' ,
+      lido: true
     },
     {
-      nome: 'incluir'
+      nome: 'O diario de Anne Frank - Anne Frank' ,
+      paginas: 224 ,
+      genero: 'autobiografia' ,
+      lido: true
     },
     {
-      nome: 'incluir'
+      nome: 'Como eu era antes de voce - Jojo Moryes' ,
+      paginas: 320 ,
+      genero: 'romance' ,
+      lido: true
     },
     {
-      nome: 'incluir'
+      nome: 'A culpa e das estrelas - John Green' ,
+      paginas: 255 ,
+      genero: 'romance' ,
+      lido: true
     },
     {
-      nome: 'incluir'
+      nome: 'Seja o amor da sua vida - Guilherme Pintto' ,
+      paginas: 224 ,
+      genero: 'autoajuda' ,
+      lido: true
     },
     {
-      nome: 'incluir'
+      nome: 'Pense de novo - Adam Grant' ,
+      paginas: 304 ,
+      genero: 'autoajuda' ,
+      lido: true
     },
     {
-      nome: 'incluir'
+      nome: 'O homem de giz - C.J.Tudor' ,
+      paginas: 272 ,
+      genero: 'terror' ,
+      lido: true
     },
+    {
+      nome: 'O cortiço - Aluisio Azevedo' ,
+      paginas: 216 ,
+      genero: 'romance' ,
+      lido: true
+    },
+    {
+      nome: 'Dom Casmurro - Machado de Assis' ,
+      paginas: 304 ,
+      genero: 'romance' ,
+      lido: true
+    },
+    {
+      nome: 'O pianista - Wladislaw Szpilman' ,
+      paginas: 224 ,
+      genero: 'drama' ,
+      lido: false
+    },
+    {
+      nome: 'A lista de Schindler - Mietetk Pemper' ,
+      paginas: 288 ,
+      genero: 'drama' ,
+      lido: false
+    },
+    {
+      nome: 'Enquanto estivermos juntos - Jeremy Camp' ,
+      paginas: 272 ,
+      genero: 'romance' ,
+      lido: false
+    }
 ]
 
 //exporta o array de objetos estudantes
--- projeto/index.js
+++ projeto/index.js
@@ -1,9 +1,48 @@
-console.log('--------------------------------------')
-console.log('     Projeto            ')
-console.log('             Tassila Bomfim         ')
-console.log('--------------------------------------')
+console.log('-------------------------------------')
+console.log('     Projeto Guiado I         ')
+console.log('         Bem vindo!          ')
+console.log('-------------------------------------')
 
 
 //vamos instanciar, o nosso banco de dados
 
+const livros = require('./database')
 
+livros.sort((a, b) => a.paginas - b.paginas)
+console.table(livros)
+
+const entry = require('readline-sync');
+
+const perguntaLivro = entry.question('Você quer ver os livros por gênero? {S/N} ').toLocaleLowerCase()
+console.log(perguntaLivro)
+
+if (perguntaLivro === 's') {
+    console.log('ESCREVA COM LETRAS MINUSCULAS E SEM ACENTO OU Ç')
+    console.log('Qual gênero você quer ler? {romance, drama, autoajuda, autobiografia, terror}.')
+
+    const generoEscolhido = entry.question('Qual gênero você quer ver?').toLocaleLowerCase()
+    console.log('Seguem os livros:')
+
+    function selecionados(livros) {
+        return livros.genero === generoEscolhido
+        
+    }
+    const filtroLivros = livros.filter(selecionados)
+    console.table(filtroLivros)
+
+
+
+const livrosLidos = entry.question('Você quer ler minhas lista de desejos?{S/N}').toLocaleLowerCase()
+console.log ('Minha lista de desejos no momento:')
+
+if (livrosLidos === 's') {
+    function livrosNaoLidos(livros) {
+        return livros.lido === false 
+        
+    }
+    const listaDesejos = livros.filter(livrosNaoLidos)
+    console.table(listaDesejos)
+}
+}else {
+    console.log('Obrigada pela visita!')
+}
